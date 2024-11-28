5088861e89e765260890c901385e984b5df0820f
qvgk <45526481+QVGK@users.noreply.github.com>
2024-11-28 14:07:01 +0800

    🔗Navbar Updates

--- src/components/Navigation/index.vue
+++ src/components/Navigation/index.vue
@@ -16,10 +16,18 @@ const NavigationItems = [
     "Terminal",
     "Help"
 ];
+
+// Nav Links
+const NavigationLinks = [
+    { href: 'https://github.com/qvgk', icon: 'simple-icons:github' },
+    { href: 'https://discord.com/users/390410425293864962', icon: 'simple-icons:discord' },
+    { href: 'https://bsky.app/profile/qvgk.org', icon: 'simple-icons:bluesky' },
+]
 </script>
 
 <template>
     <nav class="OuterContainer h-10">
+        <!-- Global Start Nav -->
         <div class="NavContainer my-3 gap-3 px-3 w-[50vw]">
             <img src="/favicon.png" class="w-4 h-4" id="NavIcon" />
             <section class="NavContainer" id="NavItems">
@@ -28,21 +36,42 @@ const NavigationItems = [
                 </button>
             </section>
         </div>
-        <div class="NavContainer !hidden md:!flex justify-center px-3 w-full">
-            <Input class="NavInput w-full xl:w-2/3" placeholder="ImVSC" disabled />
+
+        <!-- PC Middle Nav -->
+        <div class="NavContainer md:!flex justify-center gap-3 px-3 w-full">
+            <div class="NavButtons hidden md:!flex justify-end text-[#5f5f5f] text-[20px] w-[140px]">
+                <Icon name="material-symbols:arrow-left-alt-rounded" />
+                <Icon name="material-symbols:arrow-right-alt-rounded" />
+            </div>
+            <Input class="NavInput w-full !hidden md:!flex xl:w-2/3" placeholder="ImVSC" disabled />
+            <div class="NavLinks hidden md:flex gap-2 text-[20px] w-[140px] ">
+                <template v-for="(link, index) in NavigationLinks" :key="index">
+                    <NuxtLink class="Centre-Row text-[#5f5f5f] hover:text-[#cccccc] transition-all" :href="link.href"
+                        target="_blank">
+                        <Icon :name="link.icon" />
+                    </NuxtLink>
+                </template>
+            </div>
         </div>
-        <div class="NavContainer justify-end h-10 w-[50vw]">
-            <button class="NavButton hover:bg-[#29292c]">
-                <Icon name="codicon:chrome-minimize" size="16" />
-            </button>
 
-            <button class="NavButton hover:bg-[#29292c]">
-                <Icon name="codicon:chrome-restore" size="16" />
-            </button>
+        <!-- PC End Nav -->
+        <div class="Centre-Row justify-end !hidden md:!flex h-10 w-[50vw]">
+            <template
+                v-for="(icon, index) in ['codicon:chrome-minimize', 'codicon:chrome-restore', 'codicon:chrome-close']"
+                :key="index">
+                <button class="NavButton hover:bg-[#29292c]" :class="{ 'hover:bg-red-600': index === 2 }">
+                    <Icon :name="icon" size="16" />
+                </button>
+            </template>
+        </div>
 
-            <button class="NavButton hover:bg-red-600">
-                <Icon name="codicon:chrome-close" size="16" />
-            </button>
+        <!-- Mobile End Nav -->
+        <div class="flex md:hidden text-[#5f5f5f] text-[20px] gap-4 px-3">
+            <template v-for="(link, index) in NavigationLinks" :key="index">
+                <NuxtLink class="Centre-Row" :href="link.href" target="_blank">
+                    <Icon :name="link.icon" />
+                </NuxtLink>
+            </template>
         </div>
     </nav>
 </template>
